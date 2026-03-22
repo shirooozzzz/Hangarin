799ecfe794b897e423e734c4178a523dcab31e5a
Denzel Andrei Bermejo <shirooozzz13@gmail.com>
2026-03-22 14:41:04 +0800

    edited something

--- hangarin_project/settings.py
+++ hangarin_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +83,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 WSGI_APPLICATION = 'hangarin_project.wsgi.application'
 
