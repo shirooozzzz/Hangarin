5b8c64b59ed872cd11c224171e22bf19ad15fcf2
Denzel Andrei Bermejo <shirooozzz13@gmail.com>
2026-04-06 23:06:54 +0800

    added pwa

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,4 +1,6 @@
 self.addEventListener('install', function (e) {
+    console.log('SW installing...');
+
     e.waitUntil(
         caches.open('projectsite-cache-v1').then(function (cache) {
             return cache.addAll([
@@ -8,13 +10,11 @@ self.addEventListener('install', function (e) {
         })
     );
 });
+
 self.addEventListener('fetch', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (response) {
             return response || fetch(e.request);
         })
     );
-});
-self.addEventListener('install', function () {
-    console.log('Service Worker Installed');
 });
